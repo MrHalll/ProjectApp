66de4f22f9bd0e58e84783e6638fe82b08e2ea9a
MrHalll <melvinhall54@gmail.com>
2021-12-22 12:00:58 +0100

    andra commit
    skapde en ny variabel

--- app/src/main/java/com/example/testprojekt/MainActivity.java
+++ app/src/main/java/com/example/testprojekt/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         int hej = 1;
+        String namn = "Melvin";
     }
 }
