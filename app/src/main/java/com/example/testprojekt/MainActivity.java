e8ba9c964c8818b8f17a553a07bfecdf7ca55fbf
Melvin Hall <melvinhall54@gmail.com>
2021-12-28 23:32:34 +0100

    trettonde commit
    Försökte fixa så man kan spara ett projekt i molnet men något som strular

--- app/src/main/java/com/example/testprojekt/MainActivity.java
+++ app/src/main/java/com/example/testprojekt/MainActivity.java
@@ -3,27 +3,47 @@ package com.example.testprojekt;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    ListView listView;
+    FloatingActionButton newProjectButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ListView listView = (ListView) findViewById(R.id.list_view);
+        listView = findViewById(R.id.list_view);
         List<String> projectList = new ArrayList<String>();
         projectList.add(new Project("Example").toString());
         projectList.add(new Project("Android projekt").toString());
         projectList.add(new Project("Djikstra projekt").toString());
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,  R.layout.list_view, R.id.item_text_view,
-                projectList);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,  R.layout.list_view, R.id.item_text_view, projectList);
         listView.setAdapter(adapter);
+
+        //Database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        //Button för att lägga till ett nytt projekt
+        newProjectButton = findViewById(R.id.newProjectButton);
+        newProjectButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                projectList.add(new Project("Nytt projekt").toString());
+                myRef.setValue("Nytt projekt");
+            }
+        });
     }
 }
