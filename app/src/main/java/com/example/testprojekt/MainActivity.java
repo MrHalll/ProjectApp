4e7510d2783e1898294cd385655382e540a86224
Melvin Hall <melvinhall54@gmail.com>
2022-01-03 11:24:30 +0100

    sextonde commit
    Nu fungerar det att skriva till Firebase

--- app/src/main/java/com/example/testprojekt/MainActivity.java
+++ app/src/main/java/com/example/testprojekt/MainActivity.java
@@ -11,6 +11,7 @@ import android.widget.Toast;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Logger;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -26,11 +27,11 @@ public class MainActivity extends AppCompatActivity {
 
         listView = findViewById(R.id.list_view);
         List<Project> projectList = new ArrayList<Project>();
-
         ArrayAdapter<Project> adapter = new ArrayAdapter<Project>(this,  R.layout.list_view, R.id.item_text_view, projectList);
 
         //Database
         FirebaseDatabase database = FirebaseDatabase.getInstance();
+        database.setLogLevel(Logger.Level.DEBUG);
         DatabaseReference myRef = database.getReference("message");
 
         //Button för att lägga till ett nytt projekt
@@ -38,7 +39,7 @@ public class MainActivity extends AppCompatActivity {
         newProjectButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                myRef.setValue("Hello");
+                myRef.setValue("Oskar");
                 Toast.makeText(MainActivity.this, "Added", Toast.LENGTH_SHORT).show();
                 projectList.add(new Project("Nytt projekt"));
                 listView.setAdapter(adapter);
