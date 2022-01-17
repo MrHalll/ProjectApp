36d3736215ccefa221bc4fa184b8cd9e383becd7
Melvin Hall <melvinhall54@gmail.com>
2022-01-17 10:56:46 +0100

    nittonde commit
    Nu fungerar det att spara samt hämta projekt-objekt från databasen

--- app/src/main/java/com/example/testprojekt/MainActivity.java
+++ app/src/main/java/com/example/testprojekt/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.testprojekt;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
@@ -16,20 +17,27 @@ import android.widget.ListView;
 import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.GenericTypeIndicator;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
     ListView listView;
     FloatingActionButton newProjectButton;
     EditText inputText;
-    List<Project> projectList;
-    ArrayAdapter<Project> adapter;
+    List<String> projectList;
+    ArrayAdapter<String> adapter;
     FirebaseDatabase database;
     DatabaseReference dbRef;
+    int projectID = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,11 +46,32 @@ public class MainActivity extends AppCompatActivity {
 
         listView = findViewById(R.id.list_view);
         projectList = new ArrayList<>();
-        adapter = new ArrayAdapter<Project>(this,  R.layout.list_view, R.id.item_text_view, projectList);
+        adapter = new ArrayAdapter<>(this,  R.layout.list_view, R.id.item_text_view, projectList);
 
         //Database
         database = FirebaseDatabase.getInstance();
-        dbRef = database.getReference();
+        dbRef = database.getReference("Projects");
+        dbRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                GenericTypeIndicator<ArrayList<Project>> type = new GenericTypeIndicator<ArrayList<Project>>() {};
+                ArrayList<Project> project = snapshot.getValue(type);
+                if (project != null) {
+                    projectList.clear();
+                    projectID = project.size();
+                    for (int i = 0; i < project.size() ; i++) {
+                        Project tempProject = project.get(i);
+                        projectList.add(tempProject.getName());
+                    }
+                    listView.setAdapter(adapter);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
 
         //Button för att lägga till ett nytt projekt
         newProjectButton = findViewById(R.id.newProjectButton);
@@ -60,12 +89,8 @@ public class MainActivity extends AppCompatActivity {
                 builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-                        Project project = new Project(inputText.getText().toString());
-                        projectList.add(project);
-                        listView.setAdapter(adapter);
-
-                        DatabaseReference projectsRef = dbRef.child("Projects");
-                        projectsRef.setValue(projectList);
+                        Project project = new Project(inputText.getText().toString(), projectID);
+                        dbRef.child(String.valueOf(projectID++)).setValue(project);
                     }
                 });
                 builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
--- app/src/main/java/com/example/testprojekt/Project.java
+++ app/src/main/java/com/example/testprojekt/Project.java
@@ -3,16 +3,22 @@ package com.example.testprojekt;
 
 public class Project {
     String name;
+    int id;
 
     public Project(){
 
     }
 
-    public Project(String name){
+    public Project(String name, int id){
         this.name = name;
+        this.id = id;
     }
 
-    public String toString(){
+    public String getName(){
         return name;
+    }
+
+    public int getID(){
+        return id;
     }
 }
